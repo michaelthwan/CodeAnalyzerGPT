ba30e1e5d07d7ee38435931cb91c194545c1daf0
Michael Wan <michaelthwan@gmail.com>
2023-04-07 14:46:51 +0800

    CSS, markdown formatting

--- analyzer/code_analyzer.py
+++ analyzer/code_analyzer.py
@@ -45,7 +45,7 @@ class CodeAnalyzerService:
             yield chatbot, history, status
 
             # ** gpt request **
-            gpt_say = yield from ChatGPTService.predict_no_ui_but_counting_down(i_say, i_say_show_user, chatbot, history=[])  # 带超时倒计时
+            gpt_say = yield from ChatGPTService.predict_no_ui_but_counting_down(i_say, i_say_show_user, chatbot, history=[])
 
             chatbot[-1] = (i_say_show_user, gpt_say)
             history.append(i_say_show_user)
@@ -59,7 +59,7 @@ class CodeAnalyzerService:
         yield chatbot, history, 'Normal'
 
         # ** gpt request **
-        gpt_say = yield from ChatGPTService.predict_no_ui_but_counting_down(i_say, i_say, chatbot, history=history)  # 带超时倒计时
+        gpt_say = yield from ChatGPTService.predict_no_ui_but_counting_down(i_say, i_say, chatbot, history=history)
 
         chatbot[-1] = (i_say, gpt_say)
         history.append(i_say)
@@ -90,3 +90,49 @@ class CodeAnalyzerService:
     @staticmethod
     def ask_question():
         pass
+
+    @staticmethod
+    def test_formatting(txt, chatbot, history):
+        msg = r"""
+程序整体功能：CodeAnalyzerGPT工程是一个用于自动化代码分析和评审的工具。它使用了OpenAI的GPT模型对代码进行分析，然后根据一定的规则和标准来评价代码的质量和合规性。
+
+程序的构架包含以下几个模块：
+
+1. CodeAnalyzerGPT: 主程序模块，包含了代码分析和评审的主要逻辑。
+
+2. analyzer: 包含了代码分析程序的具体实现。
+
+每个文件的功能可以总结为下表：
+
+| 文件名 | 功能描述 |
+| --- | --- |
+| C:\github\!CodeAnalyzerGPT\CodeAnalyzerGPT\CodeAnalyzerGPT.py | 主程序入口，调用各种处理逻辑和输出结果 |
+| C:\github\!CodeAnalyzerGPT\CodeAnalyzerGPT\analyzer\code_analyzer.py | 代码分析器，包含了对代码文本的解析和分析逻辑 |
+| C:\github\!CodeAnalyzerGPT\CodeAnalyzerGPT\analyzer\code_segment.py | 对代码文本进行语句和表达式的分段处理 |
+
+
+Overall, this program consists of the following files:
+- `main.py`: This is the primary script of the program which uses NLP to analyze and summarize Python code.
+- `model.py`: This file defines the `CodeModel` class that is used by `main.py` to model the code as graphs and performs operations on them.
+- `parser.py`: This file contains custom parsing functions used by `model.py`.
+- `test/`: This directory contains test scripts for `model.py` and `util.py`
+- `util.py`: This file provides utility functions for the program such as getting the root directory of the project and reading configuration files.
+
+| File | Functionality |
+|------|---------------|
+| main.py | Provides the primary script for the program. Analyzes and summarizes Python code using NLP. |
+| model.py | Defines the `CodeModel` class that models the code as graphs and performs operations on them. |
+| parser.py | Contains custom parsing functions used by `model.py`. |
+| test/ | Contains test scripts for `model.py` and `util.py` |
+| util.py | Provides utility functions such as getting the root directory of the project and reading configuration files. |
+
+`util.py` specifically has two functions:
+
+| Function | Input | Output | Functionality |
+|----------|-------|--------|---------------|
+| `get_project_root()` | None | String containing the path of the parent directory of the script itself | Finds the path of the parent directory of the script itself |
+| `get_config()` | None | Dictionary containing the contents of `config.yaml` and `config_secret.yaml`, merged together (with `config_secret.yaml` overwriting any keys with the same name in `config.yaml`) | Reads and merges two YAML configuration files (`config.yaml` and `config_secret.yaml`) located in the `config` directory in the parent directory of the script. Returns the resulting dictionary. |The above material has been written in C:\github\!CodeAnalyzerGPT\CodeAnalyzerGPT\analyzer_logs\chatGPT_report2023-04-07-14-11-55.md
+
+    """
+        chatbot.append(("test prompt query", msg))
+        yield chatbot, history, 'Normal'
--- analyzer/gradio_ui_service.py
+++ analyzer/gradio_ui_service.py
@@ -1,4 +1,5 @@
 import gradio as gr
+import markdown
 
 from analyzer.code_analyzer import CodeAnalyzerService
 
@@ -10,16 +11,214 @@ functions = {
     },
     "Ask": {
         "function": CodeAnalyzerService.ask_question
-    }
+    },
+    "Test formatting": {
+        "function": CodeAnalyzerService.test_formatting
+    },
 }
 
 cancel_handles = []
 
 
 class GradioUIService:
+    def format_io(self, y):
+        """
+        Convert the input and output to HTML format.
+            Paragraphize the input part of the last item in y,
+            and convert the Markdown and mathematical formula in the output part to HTML format.
+        """
+
+        def text_divide_paragraph(text):
+            """
+            Separate the text according to the paragraph separator and generate HTML code with paragraph tags.
+            """
+            if '```' in text:
+                return text
+            else:
+                lines = text.split("\n")
+                for i, line in enumerate(lines):
+                    lines[i] = lines[i].replace(" ", "&nbsp;")
+                text = "</br>".join(lines)
+                return text
+
+        def close_up_code_segment_during_stream(gpt_reply):
+            """
+            Handling when the GPT output is cut in half
+            Add '```' at the end of the output if the output is not complete.
+            """
+            # guard pattern for normal cases
+            if '```' not in gpt_reply:
+                return gpt_reply
+            if gpt_reply.endswith('```'):
+                return gpt_reply
+
+            # otherwise
+            segments = gpt_reply.split('```')
+            n_mark = len(segments) - 1
+            if n_mark % 2 == 1:
+                return gpt_reply + '\n```'
+            else:
+                return gpt_reply
+
+        def markdown_convertion(txt):
+            """
+            Convert markdown text to HTML format. If there are math formulas, convert them to HTML format first.
+            """
+            pre = '<div class="markdown-body">'
+            suf = '</div>'
+            if ('$' in txt) and ('```' not in txt):
+                return pre + markdown.markdown(txt, extensions=['fenced_code', 'tables']) + '<br><br>' + markdown.markdown(convert_math(txt, splitParagraphs=False),
+                                                                                                                           extensions=['fenced_code', 'tables']) + suf
+            else:
+                return pre + markdown.markdown(txt, extensions=['fenced_code', 'tables']) + suf
+
+        if y is None or y == []: return []
+        i_ask, gpt_reply = y[-1]
+        i_ask = text_divide_paragraph(i_ask)
+        gpt_reply = close_up_code_segment_during_stream(gpt_reply)
+        y[-1] = (
+            None if i_ask is None else markdown.markdown(i_ask, extensions=['fenced_code', 'tables']),
+            None if gpt_reply is None else markdown_convertion(gpt_reply)
+        )
+        return y
+
+    @staticmethod
+    def get_theme():
+        try:
+            # color_er = gr.themes.utils.colors.pink
+            set_theme = gr.themes.Default(
+                primary_hue=gr.themes.utils.colors.orange,
+                neutral_hue=gr.themes.utils.colors.gray,
+                font=["Helvetica Neue", "Helvetica", "Arial", "sans-serif"],
+                font_mono=[gr.themes.GoogleFont("JetBrains Mono"), "Consolas", "ui-monospace", "monospace"]
+            )
+            # set_theme.set(
+            #     # Colors
+            #     input_background_fill_dark="*neutral_800",
+            #     # Transition
+            #     button_transition="none",
+            #     # Shadows
+            #     button_shadow="*shadow_drop",
+            #     button_shadow_hover="*shadow_drop_lg",
+            #     button_shadow_active="*shadow_inset",
+            #     input_shadow="0 0 0 *shadow_spread transparent, *shadow_inset",
+            #     input_shadow_focus="0 0 0 *shadow_spread *secondary_50, *shadow_inset",
+            #     input_shadow_focus_dark="0 0 0 *shadow_spread *neutral_700, *shadow_inset",
+            #     checkbox_label_shadow="*shadow_drop",
+            #     block_shadow="*shadow_drop",
+            #     form_gap_width="1px",
+            #     # Button borders
+            #     input_border_width="1px",
+            #     input_background_fill="white",
+            #     # Gradients
+            #     stat_background_fill="linear-gradient(to right, *primary_400, *primary_200)",
+            #     stat_background_fill_dark="linear-gradient(to right, *primary_400, *primary_600)",
+            #     error_background_fill=f"linear-gradient(to right, {color_er.c100}, *background_fill_secondary)",
+            #     error_background_fill_dark="*background_fill_primary",
+            #     checkbox_label_background_fill="linear-gradient(to top, *neutral_50, white)",
+            #     checkbox_label_background_fill_dark="linear-gradient(to top, *neutral_900, *neutral_800)",
+            #     checkbox_label_background_fill_hover="linear-gradient(to top, *neutral_100, white)",
+            #     checkbox_label_background_fill_hover_dark="linear-gradient(to top, *neutral_900, *neutral_800)",
+            #     button_primary_background_fill="linear-gradient(to bottom right, *primary_100, *primary_300)",
+            #     button_primary_background_fill_dark="linear-gradient(to bottom right, *primary_500, *primary_600)",
+            #     button_primary_background_fill_hover="linear-gradient(to bottom right, *primary_100, *primary_200)",
+            #     button_primary_background_fill_hover_dark="linear-gradient(to bottom right, *primary_500, *primary_500)",
+            #     button_primary_border_color_dark="*primary_500",
+            #     button_secondary_background_fill="linear-gradient(to bottom right, *neutral_100, *neutral_200)",
+            #     button_secondary_background_fill_dark="linear-gradient(to bottom right, *neutral_600, *neutral_700)",
+            #     button_secondary_background_fill_hover="linear-gradient(to bottom right, *neutral_100, *neutral_100)",
+            #     button_secondary_background_fill_hover_dark="linear-gradient(to bottom right, *neutral_600, *neutral_600)",
+            #     button_cancel_background_fill=f"linear-gradient(to bottom right, {color_er.c100}, {color_er.c200})",
+            #     button_cancel_background_fill_dark=f"linear-gradient(to bottom right, {color_er.c600}, {color_er.c700})",
+            #     button_cancel_background_fill_hover=f"linear-gradient(to bottom right, {color_er.c100}, {color_er.c100})",
+            #     button_cancel_background_fill_hover_dark=f"linear-gradient(to bottom right, {color_er.c600}, {color_er.c600})",
+            #     button_cancel_border_color=color_er.c200,
+            #     button_cancel_border_color_dark=color_er.c600,
+            #     button_cancel_text_color=color_er.c600,
+            #     button_cancel_text_color_dark="white",
+            # )
+        except Exception as e:
+            set_theme = None
+            print(f'please upgrade to newer version of gradio {e}')
+        return set_theme
+
+    @staticmethod
+    def get_css():
+        css = """
+/* Set the outer margins of the table to 1em, merge the borders between internal cells, and display empty cells. */
+.markdown-body table {
+    margin: 1em 0;
+    border-collapse: collapse;
+    empty-cells: show;
+}
+
+/* Set the inner margin of the table cell to 5px, the border thickness to 1.2px, and the color to --border-color-primary. */
+.markdown-body th, .markdown-body td {
+    border: 1.2px solid var(--border-color-primary);
+    padding: 5px;
+}
+
+/* Set the table header background color to rgba(175,184,193,0.2) and transparency to 0.2. */
+.markdown-body thead {
+    background-color: rgba(175,184,193,0.2);
+}
+
+/* Set the padding of the table header cell to 0.5em and 0.2em. */
+.markdown-body thead th {
+    padding: .5em .2em;
+}
+
+/* Remove the default padding of the list prefix to align it with the text line. */
+.markdown-body ol, .markdown-body ul {
+    padding-inline-start: 2em !important;
+}
+
+/* Set the style of the chat bubble, including the radius, the maximum width, and the shadow. */
+[class *= "message"] {
+    border-radius: var(--radius-xl) !important;
+    /* padding: var(--spacing-xl) !important; */
+    /* font-size: var(--text-md) !important; */
+    /* line-height: var(--line-md) !important; */
+    /* min-height: calc(var(--text-md)*var(--line-md) + 2*var(--spacing-xl)); */
+    /* min-width: calc(var(--text-md)*var(--line-md) + 2*var(--spacing-xl)); */
+}
+[data-testid = "bot"] {
+    max-width: 95%;
+    /* width: auto !important; */
+    border-bottom-left-radius: 0 !important;
+}
+[data-testid = "user"] {
+    max-width: 100%;
+    /* width: auto !important; */
+    border-bottom-right-radius: 0 !important;
+}
+
+/* Set the background of the inline code to light gray, set the radius and spacing. */
+.markdown-body code {
+    display: inline;
+    white-space: break-spaces;
+    border-radius: 6px;
+    margin: 0 2px 0 2px;
+    padding: .2em .4em .1em .4em;
+    background-color: rgba(175,184,193,0.2);
+}
+/* Set the style of the code block, including the background color, the inner and outer margins, and the radius. */
+.markdown-body pre code {
+    display: block;
+    overflow: auto;
+    white-space: pre;
+    background-color: rgba(175,184,193,0.2);
+    border-radius: 10px;
+    padding: 1em;
+    margin: 1em 2em 1em 0.5em;
+}
+"""
+        return css
+
     @staticmethod
     def get_gradio_ui():
-        with gr.Blocks() as demo:
+        gr.Chatbot.postprocess = GradioUIService.format_io
+        with gr.Blocks(theme=GradioUIService.get_theme(), css=GradioUIService.get_css()) as demo:
             gr.HTML(title_html)
             with gr.Row().style(equal_height=True):
                 with gr.Column(scale=1):
@@ -31,6 +230,8 @@ class GradioUIService:
                         txt_qa = gr.Textbox(show_label=False, placeholder="Ask questions").style(container=False)
                     with gr.Row():
                         functions["Ask"]["btn"] = gr.Button("Ask", variant="primary")
+                    with gr.Row():
+                        functions["Test formatting"]["btn"] = gr.Button("Test formatting")
                     with gr.Row():
                         reset_btn = gr.Button("Reset", variant="secondary")
                         reset_btn.style(size="sm")
